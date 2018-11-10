7919f00cd5028548e77507ca98d0776afe462c01
TJ Zimmerman <tj@tjzimmerman.com>
2018-11-10 13:53:32 -0800

    Added script to ensure apt has completed checking for first-boot updates before continuing. Moved templates to subdirectory.

---
+++ files/bash_scripts/monitor_apt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash  
+#Debian automatically checks for updates on first boot. This ensures that has completed before continuing.
+#If it hasn't finished in 5 minutes, the script will exit ungracefully.
+timeout=$(($(date +%s) + 5))
+
+while pgrep apt > /dev/null; do
+   
+    time=$(date +%s)
+
+    if [[ $time -ge $timeout ]];
+    then
+        exit 1
+    fi
+
+    sleep 1
+done;
+exit 0
