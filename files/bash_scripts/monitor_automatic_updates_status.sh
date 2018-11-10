f43fee704c70783a8a5098778674422de96cf87d
TJ Zimmerman <tj@tjzimmerman.com>
2018-11-10 14:21:54 -0800

    Increased pgrep apt timeout.

--- files/bash_scripts/monitor_automatic_updates_status.sh
+++ files/bash_scripts/monitor_automatic_updates_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
 #Debian automatically checks for updates on first boot. This ensures that has completed before continuing.
 #If it hasn't finished in 5 minutes, the script will exit ungracefully.
-timeout=$(($(date +%s) + 10))
+timeout=$(($(date +%s) + 300))
 
 while pgrep apt > /dev/null; do
    
